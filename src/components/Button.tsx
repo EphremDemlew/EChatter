eff831039e091a1b558f2ac98e63a0fd48ee0e8d
Ephrem <ephyaabb@gmail.com>
2023-05-26 14:11:19 +0300

    Modal component added

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -22,23 +22,18 @@ const Button: React.FC<ButtonProps> = ({
       disabled={disabled}
       onClick={onClick}
       className={`
-disabled:opacity-70
-disabled:cursor-not-allowed
-rounded-full 
-font-semibold
-hover:opacity-80
-transition
-border-2
-${fullWidth ? "w-full" : "w-fit"}
-${
-  secondary
-    ? "bg-white text-black border-black"
-    : "bg-sky-500 border-sky-500 text-white"
-}
-${large ? "text-xl px-5 py-3" : "text-md px-4 py-2"}
-${outline ? "bg-transparent border-r text-white" : ""}
+                    disabled:opacity-70 disabled:cursor-not-allowed rounded-full font-semibold hover:opacity-80 transition border-2 ${
+                      fullWidth ? "w-full" : "w-fit"
+                    }
+                    ${
+                      secondary
+                        ? "bg-white text-black border-black"
+                        : "bg-sky-500 border-sky-500 text-white"
+                    }
+                    ${large ? "text-xl px-5 py-3" : "text-md px-4 py-2"}
+                    ${outline ? "bg-transparent border-r text-white" : ""}
 
-  `}
+                `}
     >
       {label}
     </button>
