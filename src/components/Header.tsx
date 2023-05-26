c3a194527d600c96591f93dc3eab3c1e312d89d6
Ephrem <ephyaabb@gmail.com>
2023-05-26 12:31:51 +0300

    the initial layout finished

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -2,6 +2,7 @@
 
 import { useRouter } from "next/navigation";
 import { useCallback } from "react";
+import { BiArrowBack } from "react-icons/bi";
 
 interface HeaderProps {
   label: string;
@@ -15,7 +16,22 @@ const Header: React.FC<HeaderProps> = ({ label, showBackArrow }) => {
     router.back();
   }, [router]);
 
-  return <div className="border-b-[1px] border-neutral-800 p-5 "> {label}</div>;
+  return (
+    <div className="border-b-[1px] border-neutral-800 p-5 ">
+      <div className="flex flex-row items-center gap-2">
+        {showBackArrow && (
+          <BiArrowBack
+            size={20}
+            onClick={handleBack}
+            color="white"
+            className="cursor-pointer hover:opacity-70 transition"
+          />
+        )}
+        <h1 className=" text-white text-xl font-semibold">{label}</h1>
+      </div>
+    </div>
+  );
+  showBackArrow;
 };
 
 export default Header;
