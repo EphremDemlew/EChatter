c21556591153da520bae212cb42b19d194b5308e
Ephrem <ephyaabb@gmail.com>
2023-05-30 20:27:51 +0300

    feat: finished adding login and sign up modal

--- src/hooks/useRegisterModal.ts
+++ src/hooks/useRegisterModal.ts
@@ -7,7 +7,7 @@ interface RegisterStore {
 }
 
 const useRegisterModal = create<RegisterStore>((set) => ({
-  isOpen: true,
+  isOpen: false,
   onOpen: () => set({ isOpen: true }),
   onClose: () => set({ isOpen: false }),
   // ^?
