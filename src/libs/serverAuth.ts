64c7fdd162e95edf54233018fd12a2dc463fa93d
Ephrem <ephyaabb@gmail.com>
2023-05-31 22:08:05 +0300

    cleaned up some codes

--- src/libs/serverAuth.ts
+++ src/libs/serverAuth.ts
@@ -1,24 +1,10 @@
 import { cookies, headers } from "next/headers";
-import { getServerSession as originalGetServerSession } from "next-auth";
 import prisma from "@/libs/prismadb";
 import { authOptions } from "@/app/api/auth/[...nextauth]/route";
+import { getSession } from "next-auth/react";
 
 const serverAuth = async (req: Request) => {
-  // const getServerSession = async () => {
-  //   const req = {
-  //     headers: Object.fromEntries(headers() as Headers),
-  //     cookies: Object.fromEntries(
-  //       cookies()
-  //         .getAll()
-  //         .map((c) => [c.name, c.value])
-  //     ),
-  //   };
-  // };
-
-  // const res = { getHeader() {}, setCookie() {}, setHeader() {} };
-
-  // @ts-ignore - The type used in next-auth for the req object doesn't match, but it still works
-  const session = await originalGetServerSession(req, authOptions);
+  const session = await getSession(req);
 
   if (!session?.user?.email) {
     throw new Error("Not signed in");
