7a17d70693342df5f6024f38fa9e621d0db136a0
Ephrem <ephyaabb@gmail.com>
2023-06-15 01:47:14 +0300

    fixed some Typo errors

--- src/app/api/edit/route.ts
+++ src/app/api/edit/route.ts
@@ -5,9 +5,9 @@ import prisma from "@/libs/prismadb";
 export default async function PATCH(req: NextRequest) {
   try {
     const { currentUser } = await serverAuth(req);
-    NextResponse;
 
     const { name, username, bio, profileImage, coverImage } = await req.json();
+
     if (!name || !username) {
       throw new Error("Missing fields");
     }
@@ -21,7 +21,6 @@ export default async function PATCH(req: NextRequest) {
 
     return NextResponse.json(updatedUser, { status: 200 });
   } catch (error) {
-    error;
     console.log("🚀 ~ file: route.ts:9 ~ PATCH ~ error:", error);
     return NextResponse.json({ error: "Error" }, { status: 400 });
   }
