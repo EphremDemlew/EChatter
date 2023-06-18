7c096e8efb617a5724df2ce0349f73e401b08d6d
Ephrem <ephyaabb@gmail.com>
2023-06-18 13:49:25 +0300

    feat: added route guarding for notification

--- src/app/notification/page.tsx
+++ src/app/notification/page.tsx
@@ -1,9 +1,14 @@
 import Header from "@/components/Header";
 import { NextPageContext } from "next";
+import { getServerSession } from "next-auth";
 import { getSession } from "next-auth/react";
+import { authOptions } from "../api/auth/[...nextauth]/route";
+import { redirect } from "next/navigation";
 
-export async function getServerSideProps(context: NextPageContext) {
+export async function getData(context: NextPageContext) {
   const session = await getSession(context);
+
+  console.log("🚀 ~ file: page.tsx:7 ~ getData ~ session:", session);
 
   if (!session) {
     return {
@@ -13,11 +18,16 @@ export async function getServerSideProps(context: NextPageContext) {
       },
     };
   }
-
-  return { props: { session } };
 }
 
-const Notifications = () => {
+const Notifications = async () => {
+  const data = await getServerSession(authOptions);
+  console.log("🚀 ~ file: page.tsx:25 ~ Notifications ~ data:", data);
+
+  if (!data) {
+    redirect("/");
+  }
+
   return (
     <>
       <Header label="Notifications" showBackArrow />
